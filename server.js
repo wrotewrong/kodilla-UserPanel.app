e35b84abb75072c954db05ad1c51df880e1be755
Andrzej Popiolek <xandrzejpopiolek@gmail.com>
2023-03-05 13:17:33 +0100

    Init project

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const hbs = require('express-handlebars');
+
+const app = express();
+
+app.engine('hbs', hbs({ extname: 'hbs', layoutsDir: './layouts', defaultLayout: 'main' }));
+app.set('view engine', '.hbs');
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/user/logged', (req, res) => {
+  res.render('logged');
+});
+
+app.get('/user/no-permission', (req, res) => {
+  res.render('noPermission');
+});
+
+app.use('/', (req, res) => {
+  res.status(404).render('notFound');
+});
+
+app.listen('8000', () => {
+  console.log('Server is running on port: 8000');
+});
